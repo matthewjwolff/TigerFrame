b83b665a3e7bb0d82e9721efb3a2c50aa240b721
Matthew Wolff <mwolff3@lsu.edu>
2016-03-31 13:42:53 -0500

    part 2 done

--- Mips/MipsFrame.java
+++ Mips/MipsFrame.java
@@ -9,7 +9,10 @@ import Frame.AccessList;
 
 public class MipsFrame extends Frame {
 
+  //number of locals allocated so far
   private int count = 0;
+  //the offset of this frame for esacped local variables. does not affect formals
+  private int offset = 0;
   public Frame newFrame(Symbol name, Util.BoolList formals) {
     Label label;
     if (name == null)
@@ -24,24 +27,43 @@ public class MipsFrame extends Frame {
   public MipsFrame() {}
   private MipsFrame(Label n, Util.BoolList f) {
     name = n;
+    //allocate the formal parameters in the frame
+    //we start our offset at 0
+    allocFormals(0,f);
   }
 
   private static final int wordSize = 4;
   public int wordSize() { return wordSize; }
 
+  @Override
   public Access allocLocal(boolean escape) { 
       //if it does NOT escape, it can go InReg
-      if(!escape) {
-          //NOTE temp keeps track of the number of allocated things in its static variable
-          return new InReg(new Temp());
+      if(escape) {
+          offset -= wordSize;
+          return new InFrame(offset);
       } else {
-          //I guess offset?
-          return new InFrame(wordSize);
+          //NOTE temp keeps track of the number of allocated things in its static variable.
+          //we don't have to worry about it here.
+          //temp represents WHICH register we're using.
+          return new InReg(new Temp());
       }
   }
   
-  //rational rose says to put this. Dont' know what it does
-  private void allocFormals() {
-      
+  //Implemented non-recursively, could possibly be done better recursively?
+  private AccessList allocFormals(int offset, Util.BoolList args) {
+      AccessList list = null;
+      Util.BoolList iterator = args;
+      while (args != null) {
+          //if this escapes
+          Access access;
+          if(args.head) {
+              access = new InFrame(offset);
+              offset += wordSize;
+          }
+          else access = new InReg(new Temp());
+          list = new AccessList(access,list);
+          args = args.tail;
+      }
+      return list;
   }
 }
